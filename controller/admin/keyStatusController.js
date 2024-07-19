11f86b46877a6e8056a5c2e41f43febe6123aefc
Makutu369 <brightdreamfuture6@gmail.com>
2024-07-19 14:28:56 +0000

    finished route to get key status by email

---
+++ controller/admin/keyStatusController.js
@@ -0,0 +1,40 @@
+import z from "zod";
+import { User } from "../../model/user.js";
+import { AccessKey } from "../../model/access_key.js";
+/**
+ * TODO:
+ * get email from query param
+ * validate email
+ * verify if a user exists with that email
+ * get the id of user from the email
+ * get the key of the user using reference id
+ * check and return status of the key
+ */
+const validateEmail = (email) => {
+  const emailSchema = z.string().email();
+  const result = emailSchema.safeParse(email);
+  return result.success;
+};
+
+const keyStatusController = async (req, res) => {
+  const { email } = req.query;
+  console.log(email);
+  const isValidEmail = validateEmail(email);
+  console.log(isValidEmail);
+  if (isValidEmail == false)
+    return res.status(400).json({ message: "Invalid email" });
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  const key = await AccessKey.findOne({ user: user._id });
+
+  if (!key) return res.status(404).json({ message: `Key not found`, user });
+
+  if (key.status !== "active")
+    return res.status(404).json({ message: "no active key found" });
+
+  res.status(200).json({ message: "key is active", key });
+};
+
+export { keyStatusController };
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,9 +2,11 @@ import express from "express";
 import { loginController } from "../controller/admin/loginController.js";
 import { keyController } from "../controller/admin/keyController.js";
 import { revokeKeyController } from "../controller/admin/revokeKeyController.js";
+import { keyStatusController } from "../controller/admin/keyStatusController.js";
 const router = express.Router();
 
 router.post("/login", loginController);
 router.get("/keys", keyController);
 router.get("/key/:id/revoke", revokeKeyController);
+router.get("/key/status", keyStatusController);
 export default router;
